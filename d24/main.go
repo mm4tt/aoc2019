d277081e5110e3149b4c8673549af3451b10ced6
Mateusz Matejczyk <mmat@google.com>
2019-12-24 10:29:46 +0100

    Day 24 - part 2

--- d24/main.go
+++ d24/main.go
@@ -20,6 +20,7 @@ func main() {
 		}
 		y++
 	}
+	init := b
 
 	b.Print()
 
@@ -31,6 +32,16 @@ func main() {
 	fmt.Printf("\nAfter %d iterations\n", len(seen))
 	b.Print()
 	fmt.Println("BiodiversityRating:", b.BiodiversityRating())
+
+	fmt.Println("\nPart2")
+	init.Print()
+	r := NewRecursiveBoards(init)
+	fmt.Println("Init # bugs: ", r.CountBugs())
+
+	for i := 0; i < 200; i++ {
+		r.Iterate()
+	}
+	fmt.Println("After 200 minutes # bugs: ", r.CountBugs())
 }
 
 const (
@@ -39,12 +50,123 @@ const (
 	EMPTY = '.'
 )
 
+type RecursiveBoards struct {
+	boards map[int]Board
+
+	iterations int
+}
+
+func NewRecursiveBoards(init Board) *RecursiveBoards {
+	return &RecursiveBoards{
+		boards:     map[int]Board{0: init},
+		iterations: 0,
+	}
+}
+
+func (r *RecursiveBoards) CountBugs() int {
+	count := 0
+	for _, b := range r.boards {
+		count += b.NumBugs()
+	}
+	return count
+}
+
+func (r *RecursiveBoards) Iterate() {
+	r.iterations++
+	r.boards[r.iterations] = newBoard()
+	r.boards[-r.iterations] = newBoard()
+
+	newBoards := make(map[int]Board)
+	for level, b := range r.boards {
+		newB := newBoard()
+
+		for y := 0; y < N; y++ {
+			for x := 0; x < N; x++ {
+				if x == 2 && y == 2 {
+					continue
+				}
+				newB.Set(x, y, b.Get(x, y))
+				nAdjacentBugs := r.numSurroundingBugs(level, x, y)
+				switch b.Get(x, y) {
+				case BUG:
+					if nAdjacentBugs != 1 {
+						newB.Set(x, y, EMPTY)
+					}
+				case EMPTY:
+					if nAdjacentBugs == 1 || nAdjacentBugs == 2 {
+						newB.Set(x, y, BUG)
+					}
+				}
+			}
+		}
+
+		newBoards[level] = newB
+	}
+	r.boards = newBoards
+}
+
+func (r *RecursiveBoards) numSurroundingBugs(level, x, y int) int {
+	ret := 0
+	b := r.boards[level]
+	for xS := -1; xS <= 1; xS++ {
+		for yS := -1; yS <= 1; yS++ {
+			if util.Abs(xS)+util.Abs(yS) != 1 {
+				continue
+			}
+			x1, y1 := x+xS, y+yS
+			if x1 == 2 && y1 == 2 {
+				continue
+			}
+			if b.isOnBoard(x1, y1) {
+				ret += b.nBugs(x1, y1)
+			}
+		}
+	}
+	if level != -r.iterations {
+		if x == 0 {
+			ret += r.boards[level-1].nBugs(1, 2)
+		}
+		if y == 0 {
+			ret += r.boards[level-1].nBugs(2, 1)
+		}
+		if x == N-1 {
+			ret += r.boards[level-1].nBugs(3, 2)
+		}
+		if y == N-1 {
+			ret += r.boards[level-1].nBugs(2, 3)
+		}
+	}
+
+	if level != r.iterations {
+		if x == 1 && y == 2 {
+			ret += r.boards[level+1].nBugsInCol(0)
+		}
+		if x == 2 && y == 1 {
+			ret += r.boards[level+1].nBugsInRow(0)
+		}
+		if x == 3 && y == 2 {
+			ret += r.boards[level+1].nBugsInCol(N - 1)
+		}
+		if x == 2 && y == 3 {
+			ret += r.boards[level+1].nBugsInRow(N - 1)
+		}
+	}
+
+	return ret
+}
+
 type Board struct {
 	s string
 }
 
 func newBoard() Board {
-	return Board{string(make([]rune, N*N))}
+	b := Board{string(make([]rune, N*N))}
+	for y := 0; y < N; y++ {
+		for x := 0; x < N; x++ {
+			b.Set(x, y, EMPTY)
+		}
+	}
+	return b
 }
 
 func (b *Board) Next() Board {
@@ -100,8 +222,18 @@ func (b *Board) index(x, y int) int {
 
 func (b *Board) Print() {
 	for i := 0; i < N; i++ {
-		fmt.Println(b.s[i : i+N])
+		fmt.Println(b.s[i*N : (i+1)*N])
 	}
+}
+
+func (b *Board) NumBugs() int {
+	count := 0
+	for i := 0; i < N*N; i++ {
+		if b.s[i] == BUG {
+			count++
+		}
+	}
+	return count
 }
 
 func (b *Board) numSurroundingBugs(pX, pY int) int {
@@ -111,12 +243,35 @@ func (b *Board) numSurroundingBugs(pX, pY int) int {
 			if util.Abs(x)+util.Abs(y) != 1 {
 				continue
 			}
-			if b.isOnBoard(pX+x, pY+y) && b.Get(pX+x, pY+y) == BUG {
-				ret++
+			if b.isOnBoard(pX+x, pY+y) {
+				ret += b.nBugs(pX+x, pY+y)
 			}
 		}
 	}
 	return ret
+}
+
+func (b Board) nBugs(x, y int) int {
+	if b.Get(x, y) == BUG {
+		return 1
+	}
+	return 0
+}
+
+func (b Board) nBugsInCol(x int) int {
+	count := 0
+	for y := 0; y < N; y++ {
+		count += b.nBugs(x, y)
+	}
+	return count
+}
+
+func (b Board) nBugsInRow(y int) int {
+	count := 0
+	for x := 0; x < N; x++ {
+		count += b.nBugs(x, y)
+	}
+	return count
 }
 
 func (b *Board) isOnBoard(x, y int) bool {
